84ef82b48348b40cb8ab0608d05f2f725afe7a95
muthu-PM <muthu@abc.com>
2025-03-04 18:37:20 +0530

    added new api - student genderwise

--- src/main/java/com/example/srmschool/controller/TeacherController.java
+++ src/main/java/com/example/srmschool/controller/TeacherController.java
@@ -1,5 +1,19 @@
 package com.example.srmschool.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.srmschool.model.Teacher;
+
 public class TeacherController {
+	
+	// here basic CRUD api are there
+	
+	@GetMapping("/api/students/gender")
+	public List<Teacher> sampleApi()
+	{
+		return null;
+	}
 
 }
